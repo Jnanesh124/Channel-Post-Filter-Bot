434e771f231c4c4ad2f91013047f446d781df05c
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-10-25 00:44:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
-API_ID       = 12124605
-API_HASH     = "5cf3577d85fd02286535ec2296934287"
+API_ID       = 29570230
+API_HASH     = "17c54577b97263cce14f01f820a7e478"
 BOT_TOKEN    = "6896769096:AAFqO1_KU8f8mMNPx9qy4Cr5pFlQwH2ZXaA"
-SESSION      = "BQEVDDgAqAeqpwh6ISGOHhApFeAW2aOQ1fgvqNm5kH5dR2DFH3AGfjSAG6gtsq4Mz2Ra6uekSou5X1Breuqjs-DTnHtHNfRVDj3RB86iRFlIF1mA7R4kHDvGF-qVFGMbUjePCSFarxFZch-AHNN4SspYH_i3KHN5h_juhSOSlT5O2iVGIq2CNp22_aPxWkifsBrQnUuyJ3tKTy7_HQ734ICQ5RATZtWtMdO0kCUbY6TyhgYJ0L8jm7AnsqnEmBdM2UkF4fuuasDrOmDocrm39vbITKNgjnucAODx2AtY8bSD5qNDx-BH_vIORzuGF72wqzU19kDzlpyb_-Uqx483blPHyKfOQgAAAAFhSwkGAA"
-DATABASE_URI = "mongodb+srv://MdiskDb:MdiskDb@cluster0.lmirmtn.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001980857896
-ADMIN        = 6051691339
+SESSION      = "BQHDNLYAbt6b93ncY_c6aHb0syyMG1ZtlZWr-h_Mfdg44-cgD9kUShzgWapVprJyh4az3voPHYA9U6iB0iY81HI265idO-lAHYYyNfwn_gTSraqLEjqMkcdsqO8D7hTgYzcvROFHaGQr12_XoiUpqZYkx3zdG0Guq728ApHoqGBYecGf_W_QfAsGzGwjHKtRCL-UPIdWkod0E0fCqv5jeMOHfz7HpQb45DhvSkwik_2-ly44tqW7I8jZGsDXHGRMG0Ue1GXwBCHHKx8dSYiooozYdk07klsT_M3tmj1xpqUDCm_WTnus2UGHxLaEbPpPkDDoAPl8qs87g6bb6p6yqwgEhn5RQAAAAAF3-xvVAA"
+DATABASE_URI = "mongodb+srv://mangodb:mangodb@cluster0.dskkdky.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001835357010
+ADMIN        = 5047601096
